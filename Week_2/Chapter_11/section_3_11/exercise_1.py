df4e8fcd582cc706e0737dc7c9bf02e34ab2601e
Marnix <m.dekker.18@student.rug.nl>
2020-04-30 10:59:51 +0200

    first half week 2

---
+++ Week_2/Chapter_11/section_3_11/exercise_1.py
@@ -0,0 +1,5 @@
+from Week_2.Chapter_11.section_3_11.mytime import MyTime
+
+tim1 = MyTime(11, 59, 30)
+
+print(tim1)
---
+++ Week_2/Chapter_11/section_3_11/mytime.py
@@ -0,0 +1,7 @@
+class MyTime:
+
+    def __init__(self, hrs=0, mins=0, secs=0):
+        self.hours = hrs
+        self.minutes = mins
+        self.seconds = secs
+
